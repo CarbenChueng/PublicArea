b967bd50bbfbda20e019a4b943c550a0e5e965dd
kabunchueng <kabunchueng@icloud.com>
2023-07-22 23:42:48 +0800

    all

--- python/PyWrite/test04.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torch import nn
-
-
-if __name__ == '__main__':
-    conv = nn.Conv2d(3,16,3,1,padding=1)
-    x = torch.randn(1,3,24,16)#NCHW
-    y = conv(x)
-    print(y.shape)
--- python/Study/LeiWei/test/111.py
+++
@@ -1,4 +0,0 @@
-from torchvision import datasets
-
-cifar10 = datasets.CIFAR10("wd",download=True)
-print(cifar10.shape)
