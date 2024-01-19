a729752f76dae186230a03c6727ac80df0e1ba4f
carben <carbenchueng@icloud.com>
2024-01-19 16:47:50 +0800

    all

--- Code/_C++Project/P03_PointCircle.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by kabun  on 10/11/21.
-//
-
-#ifndef INC_3_CPP_P03_POINTCIRCLE_H
-#define INC_3_CPP_P03_POINTCIRCLE_H
-
-#endif //INC_3_CPP_P03_POINTCIRCLE_H
--- Code/_PythonProject/_05_MTCNN/_05_ONet.py
+++
@@ -1,8 +0,0 @@
-from _05_Net import ONet
-from _05_Train import Trainer
-
-
-if __name__ == '__main__':
-    o_net = ONet()
-    trainer = Trainer(o_net, "parameter/onet.pt", "/Users/carbenchueng/Desktop/2-Data/Celeba/target/48")
-    trainer.train()
